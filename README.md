a89ab6d05a73519ec2facb9295f7214ee847c756
VanNam2000 <vannam2072000@gmail.com>
2022-11-26 21:35:37 +0700

    upload source code

---
+++ README.md
@@ -0,0 +1,6 @@
+# CaseStudy_Md1_C1022I1
+link img:
+./assets/img/imgAddProduct/caffee1.webp
+./assets/img/imgAddProduct/caffee2.webp
+./assets/img/imgAddProduct/caffee3.webp
+./assets/img/imgAddProduct/caffee4.webp
---
+++ assets/js/login.js
@@ -0,0 +1,35 @@
+const userName = "NAM TEA";
+const userPassword = "123456";
+let inputUserName = document.getElementById("name");
+let inputUserPassword = document.getElementById("password");
+let modal = document.getElementById("modal");
+
+function openLoginModal() {
+  modal.style.display = "flex";
+}
+
+function closeLoginModal() {
+  modal.style.display = "none";
+}
+
+// Login Admin
+function acceptLogin() {
+  if (
+    inputUserName.value == userName &&
+    inputUserPassword.value == userPassword
+  ) {
+    let result = "";
+    result += `<i class="fa-solid fa-user"></i>
+                   <input id="userName" class="header__notify-right-items-input" value="Tài khoản" readonly></input>`;
+    document.getElementById("account").innerHTML = result;
+    document.getElementById("userName").value = userName;
+    displayHotTea();
+  } else {
+    alert("Sai tên đăng nhập hoặc mật khẩu!");
+  }
+}
+
+function clickLogin() {
+  acceptLogin();
+  closeLoginModal();
+}
